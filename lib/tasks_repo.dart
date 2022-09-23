a48a2c83a701d19ab91cbb18dc6378156311edbc
Loc <ttloc1999@gmail.com>
2022-09-23 14:35:57 +0700

    add delete all method

--- lib/tasks_repo.dart
+++ lib/tasks_repo.dart
@@ -11,7 +11,11 @@ class TasksRepo {
 
   Stream<List<Task>> getTasks() => _taskApi.getTasksStream();
 
+  Future<List<Task>> getTasksFuture() => _taskApi.getTasks();
+
   Future<void> saveTask(Task task) => _taskApi.saveTask(task);
 
   Future<void> deleteTask(String id) => _taskApi.deleteTask(id);
+
+  Future<void> deleteAll() => _taskApi.deleteAll();
 }
