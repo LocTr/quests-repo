d88097976c446a308fb2818b3786a877c95c0b1f
Loc <ttloc1999@gmail.com>
2022-09-28 16:58:16 +0700

    split repos code

---
+++ lib/src/tasks_repo.dart
@@ -0,0 +1,18 @@
+import 'package:task_api/models/task.dart';
+import 'package:task_api/task_api.dart';
+
+class TasksRepo {
+  const TasksRepo({required TaskApi taskApi}) : _taskApi = taskApi;
+
+  final TaskApi _taskApi;
+
+  Stream<List<Task>> getTasks() => _taskApi.getTasksStream();
+
+  Future<List<Task>> getTasksFuture() => _taskApi.getTasks();
+
+  Future<void> saveTask(Task task) => _taskApi.saveTask(task);
+
+  Future<void> deleteTask(String id) => _taskApi.deleteTask(id);
+
+  Future<void> deleteAll() => _taskApi.deleteAll();
+}
--- lib/tasks_repo.dart
+++ lib/tasks_repo.dart
@@ -1,21 +1,3 @@
 library tasks_repo;
 
-import 'package:task_api/models/task.dart';
-import 'package:task_api/task_api.dart';
-
-/// A Calculator.
-class TasksRepo {
-  const TasksRepo({required TaskApi taskApi}) : _taskApi = taskApi;
-
-  final TaskApi _taskApi;
-
-  Stream<List<Task>> getTasks() => _taskApi.getTasksStream();
-
-  Future<List<Task>> getTasksFuture() => _taskApi.getTasks();
-
-  Future<void> saveTask(Task task) => _taskApi.saveTask(task);
-
-  Future<void> deleteTask(String id) => _taskApi.deleteTask(id);
-
-  Future<void> deleteAll() => _taskApi.deleteAll();
-}
+export 'src/tasks_repo.dart';
