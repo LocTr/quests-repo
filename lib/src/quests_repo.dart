15aedc6de38cbe3f2b16a3818a33d5c18c8d406a
Loc <ttloc1999@gmail.com>
2022-11-13 20:40:49 +0700

    update repo include Goals

--- lib/src/quests_repo.dart
+++ lib/src/quests_repo.dart
@@ -1,11 +1,17 @@
+import 'package:quests_api/apis/goals_api.dart';
 import 'package:quests_api/apis/quests_api.dart';
+import 'package:quests_api/models/goal.dart';
 import 'package:quests_api/models/quest.dart';
 
 class QuestsRepo {
-  const QuestsRepo({required QuestsApi questsApi}) : _questsApi = questsApi;
+  const QuestsRepo({required QuestsApi questsApi, required GoalsApi goalsApi})
+      : _questsApi = questsApi,
+        _goalsApi = goalsApi;
 
   final QuestsApi _questsApi;
+  final GoalsApi _goalsApi;
 
+  //Quests
   Stream<List<Quest>> getQuests() => _questsApi.getQuestsStream();
 
   Future<List<Quest>> getQuestsFuture() => _questsApi.getQuests();
@@ -14,5 +20,14 @@ class QuestsRepo {
 
   Future<void> deleteQuest(String id) => _questsApi.deleteQuest(id);
 
-  Future<void> deleteAll() => _questsApi.deleteAllQuests();
+  Future<void> deleteAllQuest() => _questsApi.deleteAllQuests();
+
+  //Goals
+  Stream<List<Goal>> getGoals() => _goalsApi.getGoalsStream();
+
+  Future<void> saveGoals(Goal goal) => _goalsApi.saveGoal(goal);
+
+  Future<void> deleteGoals(String id) => _goalsApi.deleteGoal(id);
+
+  Future<void> deleteAllGoal() => _goalsApi.deleteAllGoals();
 }
